871b78004f3300fe181e09614e1c2a473f0c30db
criskell <cris.moraes.ds@gmail.com>
2025-12-07 10:22:56 -0300

    compile string literals

--- mem.h
+++ mem.h
@@ -3,6 +3,9 @@
 
 #include "common.h"
 
+#define ALLOCATE(type, count) \
+  (type*)reallocate(NULL, 0, sizeof(type) * count)
+
 // It avoids unnecessary relocations and copies at the beginning of the object.
 #define GROW_CAPACITY(capacity) \
   ((capacity < 8) ? 8 : (capacity * 2))
