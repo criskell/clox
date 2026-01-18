6485e219bb2a6602204baf6d25d2bc183f4fe3fe
criskell <cris.moraes.ds@gmail.com>
2026-01-18 06:24:31 -0300

    track local scopes and compile blocks

--- compiler.c
+++ compiler.c
@@ -62,8 +62,21 @@ typedef struct {
   // This is a anonymous struct.
 } ParseRule;
 
+typedef struct {
+  Token name;
+  int depth;
+} Local;
+
+typedef struct {
+  Local locals[UINT8_COUNT];
+  int localCount;
+  int scopeDepth;
+} Compiler;
+
 // A pointer to the parser's state.
 Parser parser;
+
+Compiler* current = NULL;
 
 // A pointer to the chunk currently being compiled.
 Chunk* compilingChunk;
@@ -174,6 +187,12 @@ static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
+static void initCompiler(Compiler* compiler) {
+  compiler->localCount = 0;
+  compiler->scopeDepth = 0;
+  current = compiler;
+}
+
 // End compiler and maybe print code.
 static void endCompiler() {
   emitReturn();
@@ -185,6 +204,16 @@ static void endCompiler() {
     printf("==\n");
   } 
 #endif
+}
+
+static void beginScope() {
+  // Create a new scope simply by increasing the scope depth. Much faster than the jlox implementation which creates a
+  // new HashMap for each scope.
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
 }
 
 static void expression();
@@ -463,6 +492,14 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
   uint8_t global = parseVariable("Expect variable name.");
 
@@ -534,6 +571,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
@@ -544,6 +585,10 @@ static void statement() {
 bool compile(const char* source, Chunk* chunk) {
   // Start the lazy lexical analysis.
   initScanner(source);
+
+  Compiler compiler;
+  initCompiler(&compiler);
+
   compilingChunk = chunk;
   parser.hadError = false;
   // Used to prevent cascading errors.
