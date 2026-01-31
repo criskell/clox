ceb48b2d3c89a13e3bc315c6ceb6c5570d611a50
criskell <cris.moraes.ds@gmail.com>
2026-01-31 17:12:10 -0300

    while statement

--- vm.c
+++ vm.c
@@ -283,6 +283,12 @@ static InterpretResult run() {
 
         break;
       }
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        
+        break;
+      }
       case OP_RETURN: {
         return INTERPRET_OK;
       }
